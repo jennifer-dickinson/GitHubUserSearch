cf828be78126a3b72b744c70e1d9341312123710
Jennifer Salas <doodle@Jennifers-MacBook.local>
2018-01-16 17:54:45 -0800

    Allowed pagination of followers. Made icons smaller. Switched css from padding-dominant to margin.

--- jsonhandling.js
+++ jsonhandling.js
@@ -1,23 +1,21 @@
-function getFollowers(followers_url, totalListed) {
-  fetch(followers_url)
+function getFollowers(followers_url, page, count, numFollowers) {
+  var show = 24;
+  fetch(followers_url + "?per_page="+ show + "&page=" + page)
     .then( function (fL) {return fL.json()})
     .then( function (users) {
       // console.log(users);
       var followers = document.getElementById('followers')
 
-      var traverse = users.length - totalListed > 10 ? 10 : users.length-totalListed;
-      var newList = totalListed + traverse;
-
-      console.log("Showing an additional " + traverse + " followers.")
-
-      for(var i = totalListed; i < newList; i++) {
+      for(var i = 0; i < users.length; i++) {
 
         var follower = document.createElement('div')
         follower.setAttribute('class', "follower");
 
         var followerImage = document.createElement('img');
         followerImage.src = users[i].avatar_url;
-        followerImage.setAttribute('class', "followerImage");
+        followerImage.setAttribute('class', "followerAvatar");
+        followerImage.setAttribute('alt', users[i].login);
+        followerImage.setAttribute('title', users[i].login);
         follower.appendChild(followerImage);
 
         var name = document.createElement('div');
@@ -26,32 +24,30 @@ function getFollowers(followers_url, totalListed) {
         follower.appendChild(name);
 
         follower.setAttribute('onClick',
-          "document.getElementById('srcUser').innerText = " + "'"
-          + users[i].login + "'; getUser();"
+          "document.getElementById('searchInput').innerText = '" + users[i].login + "'; getUser();"
         );
-        // follower.innerText+=users[i].login;
         followers.appendChild(follower);
       }
-      if (newList >= users.length && totalListed != 0) {
+
+      if (count + users.length < numFollowers) {
+          if (count > 0) {
+            var more = document.getElementById('MoreLink');
+            more.parentElement.removeChild(more);
+          }
+          else {
+            var more = document.createElement('div');
+            more.setAttribute('id', "MoreLink");
+            more.innerText = "More..."
+          }
+          count += users.length;
+          more.setAttribute('onClick',
+          "getFollowers('" + followers_url +  "'," + (page+1) + "," + count + "," + numFollowers + ");" );
+          document.getElementById('followers').appendChild(more);
+        }
+      else if (count > 0) {
         var more = document.getElementById('MoreLink');
         more.parentElement.removeChild(more);
-      }
-      else if (newList < users.length) {
-        if (totalListed === 0) {
-          var more = document.createElement('div');
-          more.setAttribute('id', "MoreLink");
-          more.innerText = "More..."
         }
-        else {
-          var more = document.getElementById('MoreLink');
-          more.parentElement.removeChild(more);
-        }
-
-        more.setAttribute('onClick',
-        "getFollowers('" + followers_url +  "'," +  newList + ");" );
-        document.getElementById('followers').appendChild(more);
-      }
-      console.log( newList + " followers shown. " + (users.length - newList) + " not shown.");
     })
 }
 
@@ -60,21 +56,19 @@ function loadUser(user) {
   console.log(user);
 
   document.getElementById('followers').innerText = "";
-  document.getElementById('srcUser').value = user.login;
+  document.getElementById('searchInput').value = user.login;
   document.getElementById('username').innerText = user.login;
-  document.getElementById('avatarIMG').src = user.avatar_url;
+  document.getElementById('userAvatar').src = user.avatar_url;
   document.getElementById('realname').innerText = user.name;
   document.getElementById('location').innerText = user.location;
   document.getElementById('bio').innerText = user.bio;
   document.getElementById('numFollowers').innerText =
     user.followers + " followers";
-
-  var numFL = 0;
-  getFollowers(user.followers_url.replace('{*}',''), numFL);
+  getFollowers(user.followers_url.replace('{*}',''), 1, 0, user.followers);
 }
 
 function getUser() {
-  name = document.getElementById('srcUser').innerText;
+  name = document.getElementById('searchInput').innerText;
   var url = 'https://api.github.com/users/' + name;
   fetch(url)
     .then(function(r) {
@@ -93,17 +87,17 @@ function getUser() {
     });
   };
 
-document.getElementById('search').addEventListener('click', function() {
+document.getElementById('searchButton').addEventListener('click', function() {
   getUser();
 });
-document.getElementById('srcUser').addEventListener('keypress', function(e) {
+document.getElementById('searchInput').addEventListener('keypress', function(e) {
   var key = e.which || e.keyCode;
   if (key === 13) {
-    // document.getElementById('srcUser').replace(/\n$/, '');
+    // document.getElementById('searchInput').replace(/\n$/, '');
     e.preventDefault();
     getUser();
   }
 });
 
-document.getElementById('srcUser').innerText = "jennifersalas";
+document.getElementById('searchInput').innerText = "jennifersalas";
 getUser();
